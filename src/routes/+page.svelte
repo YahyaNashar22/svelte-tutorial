5d33b4b56ac2ea187a35b7712058c4f4f299a52f
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2026-02-10 23:43:23 +0200

    props - states - derived

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Header from './Header.svelte';
+
+	let name = $state('Yahya');
+
+	let color = 'blueviolet';
+
+	let status: 'OPEN' | 'CLOSED' = $state('OPEN');
+
+	let full_name = $derived(name + ' ' + 'is black');
+
+	function toggle() {
+		status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
+	}
+</script>
+
+<Header {name} {color} />
+
+<input type="text" bind:value={name} />
+
+<p>The store is now {status}</p>
+
+<button onclick={toggle}>Toggle Status</button>
+
+<h2>{full_name}</h2>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	let { name, color } = $props();
+</script>
+
+<h1 style="color: {color}">Hello From {name}</h1>
+
+<style>
+	h1 {
+		font-size: 2.4rem;
+		font-weight: bold;
+	}
+</style>
